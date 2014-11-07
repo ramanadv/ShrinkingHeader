7c7cb2b036bb9385111164f28b3ed9c371bc42cf
Your Name <you@example.com>
2014-11-07 14:14:57 +0530

    first commit

---
+++ src/com/shrinkingHeader/MainActivity.java
@@ -0,0 +1,83 @@
+package com.shrinkingHeader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	ArrayAdapter<String> adapter;
+	ListView list;
+	TextView price;
+	View firstChildInList;
+	boolean FIRST_TIME = true;
+	private int screenHeight;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		list = (ListView)findViewById(R.id.list);
+		price = (TextView)findViewById(R.id.price);
+		
+		//populate list items
+		String[] listItems = null ;
+		listItems = new String[30];
+		for(int i=0;i<30;i++){
+			listItems[i] = "list Item : "+(i+1);
+		}
+		
+		// set the height of priceTextView to half of the screen
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		setHeightForView(price, metrics.heightPixels/2-getActionBar().getHeight());
+		screenHeight = metrics.heightPixels;
+		
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+		list.setAdapter(adapter);
+
+		
+		list.setOnScrollListener(new OnScrollListener() {
+			
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+			}
+			
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,int visibleItemCount, int totalItemCount) {
+				int scroll = getScroll(list);
+				changeHeight(price,scroll);
+				System.out.println("scroll "+scroll);
+			}
+		});
+		
+	}
+
+	protected int getScroll(ListView listView) {
+		firstChildInList = listView.getChildAt(0);
+		if(firstChildInList == null)return 0;
+		return -firstChildInList.getTop() + listView.getFirstVisiblePosition() * firstChildInList.getHeight();
+	}
+
+	protected void setHeightForView(View v ,int h){
+		LinearLayout.LayoutParams params = (LinearLayout.LayoutParams)v.getLayoutParams(); 
+		params.height = h;
+		v.setLayoutParams(params);
+	}
+	protected void changeHeight(View view,int scroll) {
+		int priceHeight = price.getHeight();
+		if(priceHeight>=screenHeight/4 && priceHeight<=screenHeight/2){
+			setHeightForView(view, screenHeight/2-scroll);
+		}
+		else if(priceHeight < screenHeight/4){
+			
+		}
+	}
+}
